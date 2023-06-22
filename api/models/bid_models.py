d1f230cd04e7ef69a97cbb6152c363c300366e9b
piratejas <piratejas@gmail.com>
2023-06-22 12:48:00 +0100

    wip: add phase info to success/failed
    Co-authored-by: Julio Velezmoro <Lakorthus@users.noreply.github.com>

--- api/models/bid_models.py
+++ api/models/bid_models.py
@@ -1,6 +1,5 @@
 from flask import request, jsonify
-from uuid import uuid4
-from api.schemas.bid_schema import BidSchema
+from api.schemas.bid_schema import BidSchema, PhaseInfo
 
 def get_test():
     return 'test', 200
@@ -14,31 +13,26 @@ def create_bid():
         if field not in request.json:
             return jsonify({'error': f'Missing mandatory field: {field}'}), 400
         
-    # BidSchema object
-    id_obj = uuid4()   
+    # BidSchema object   
     bid_schema = BidSchema(
-        id=id_obj,
         tender=request.json['tender'],
         client=request.json['client'],
         alias=request.json.get('alias', ''),
         bid_date=request.json['bid_date'],
         bid_folder_url=request.json.get('bid_folder_url', ''),
         status='in-progress',
-        links={
-            'self': f"/bids/{id_obj}",
-            'questions': f"/bids/{id_obj}/questions"
-        },
-        was_successful=request.json.get('was_successful', ''),
-        success=request.json.get('success', []),
+        
         failed=request.json.get('failed', {}),
-        feedback=request.json.get('feedback', {}),
-        last_updated=''
+        feedback=request.json.get('feedback', {})
     )
+    success_phase =  PhaseInfo()
+    bid_schema.success.append(success_phase(1, True, 2, 3))
+    bid_schema.was_successful = bid_schema.failed == {} 
     # Convert the mock BidSchema object to a dictionary
     bid_json = bid_schema.toDbCollection()
     
     # Save data in memory
-    f=open('test.txt','a')
+    f=open('./test.txt','a')
     f.write(str(bid_json))
     f.close
 
--- api/schemas/bid_schema.py
+++ api/schemas/bid_schema.py
@@ -1,19 +1,25 @@
+from uuid import uuid4
+from datetime import datetime
+
 # Description: Schema for the bid object
 class BidSchema:
-    def __init__(self, id, tender, client, bid_date, alias='', bid_folder_url='', status='in-progress', links=None, was_successful='', success=None, failed=None, feedback=None, last_updated=''):
-        self.id = id
+    def __init__(self, tender, client, bid_date, alias='', bid_folder_url='', status='in-progress', was_successful=False, failed=None, feedback=None):
+        self.id = uuid4()
         self.tender = tender
         self.client = client
         self.alias = alias
-        self.bid_date = bid_date
+        self.bid_date = datetime.strptime(bid_date, '%d-%m-%Y').isoformat() # DD-MM-YYYY
         self.bid_folder_url = bid_folder_url
         self.status = status # enum: "deleted", "in-progress" or "completed"
-        self.links = links
+        self.links = {
+            'self': f"/bids/{self.id}",
+            'questions': f"/bids/{self.id}/questions"
+        }
         self.was_successful = was_successful
-        self.success = success
+        self.success = [] 
         self.failed = failed
         self.feedback = feedback
-        self.last_updated = last_updated
+        self.last_updated = datetime.now().isoformat()
 
     def toDbCollection(self):
         return {
@@ -26,8 +32,16 @@ class BidSchema:
             "status": self.status, 
             "links": self.links,  
             "was_successful": self.was_successful,
-            "success": self.success,
+            "success": [s.__dict__ for s in self.success] if self.success else None,
             "failed": self.failed,
             "feedback": self.feedback,
             "last_updated": self.last_updated
         }
+    
+# Schema for phaseInfo object
+class PhaseInfo:
+    def __init__(self, phase, has_score, score=None, out_of=None):
+            self.phase = phase
+            self.has_score = has_score
+            self.score = score
+            self.out_of = out_of
