5f10e678072f93acd500b0cd0d492dadf52ea995
piratejas <piratejas@gmail.com>
2023-06-27 17:02:51 +0100

    refactor: swagger config components and refs
    Co-authored-by: Julio Velezmoro <Lakorthus@users.noreply.github.com>

--- api/schemas/phase_schema.py
+++ api/schemas/phase_schema.py
@@ -1,6 +1,6 @@
 # Schema for phaseInfo object
 class PhaseInfo:
-    def __init__(self, phase, has_score, score=None, out_of=None):
+    def __init__(self, phase, has_score, score=int, out_of=int):
             self.phase = phase
             self.has_score = has_score
             self.score = score
