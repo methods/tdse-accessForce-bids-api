f4a0bf408c37483dd482d92920443a9a8fe491ce
piratejas <piratejas@gmail.com>
2023-07-05 14:53:38 +0100

    refactor: removed unused imports and added comments

--- api/controllers/bid_controller.py
+++ api/controllers/bid_controller.py
@@ -3,7 +3,7 @@ from marshmallow import ValidationError
 from api.schemas.bid_schema import BidSchema
 from api.schemas.bid_request_schema import BidRequestSchema
 from dbconfig.mongo_setup import dbConnection
-from pymongo.errors import ConnectionFailure, PyMongoError
+from pymongo.errors import ConnectionFailure
 from helpers.helpers import showConnectionError, showNotFoundError
 
 
@@ -15,29 +15,34 @@ def get_bids():
 
 @bid.route("/bids/<bid_id>", methods=["GET"])
 def get_bid_by_id(bid_id):
+    # Returns bid document where _id is equal to bid_id argument
     try:
         db = dbConnection()
         data = db['bids'].find_one({"_id": bid_id})
+        # Return 404 response if not found / returns None
         if data is None:
             return showNotFoundError()
         return data, 200
+    # Return 500 response in case of connection failure
     except ConnectionFailure:
         return showConnectionError()
 
 @bid.route("/bids", methods=["POST"])
 def post_bid():
-    # Create bid document and return error if input validation fails
+    # Create bid document and inserts it into collection
     try:
         db = dbConnection()
         bid_document = BidRequestSchema().load(request.json)
-        # Serialize to a JSON-encoded string
+        # Serialize to a JSON object
         data = BidSchema().dump(bid_document)
         # Insert document into database collection
         bids = db['bids']
         bids.insert_one(data)
         return data, 201
+    # Return 400 response if inout validation fails
     except ValidationError as e:
         return jsonify({"Error": str(e)}), 400
+    # Return 500 response in case of connection failure
     except ConnectionFailure:
         return showConnectionError()
 
