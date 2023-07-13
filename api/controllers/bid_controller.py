49ad68a1433b77d085f62d53e047482a763de10c
Lakorthus <julio28ve@gmail.com>
2023-07-13 10:34:26 +0100

    refactor: testing without magicMock

--- api/controllers/bid_controller.py
+++ api/controllers/bid_controller.py
@@ -36,9 +36,8 @@ def get_bid_by_id(bid_id):
     # Get bid by id from database collection
     try:
         db = dbConnection()
-        data = db['bids'].find_one({"_id": bid_id})
+        data = db['bids'].find_one({"_id": bid_id , "status": {"$ne": Status.DELETED.value}})
         # Return 404 response if not found / returns None
-
         if data is None:
             return showNotFoundError()
         return data, 200
@@ -57,8 +56,8 @@ def change_status_to_deleted(bid_id):
     
     try:
         db = dbConnection()
-        data = db['bids'].find_one({"_id": bid_id})
-        if data is None or data['status'] == Status.DELETED.value:
+        data = db['bids'].find_one({"_id": bid_id , "status": {"$ne": Status.DELETED.value}})
+        if data is None:
             return showNotFoundError()
         else:
             db['bids'].update_one({"_id": bid_id}, {"$set": {"status": Status.DELETED.value}})
@@ -75,7 +74,6 @@ def post_bid():
     # Create bid document and inserts it into collection
     try:
         db = dbConnection()
-        # Deserialize and validate request against schema
         # Process input and create data model
         bid_document = BidRequestSchema().load(request.get_json())
         # Serialize to a JSON object
--- tests/test_delete_bid.py
+++ tests/test_delete_bid.py
@@ -2,11 +2,8 @@ from flask import Flask
 import pytest
 from api.controllers.bid_controller import bid
 from pymongo.errors import ConnectionFailure
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch
 from marshmallow import ValidationError
-
-
-
 
 @pytest.fixture
 def client():
@@ -14,44 +11,33 @@ def client():
     app.register_blueprint(bid, url_prefix='/api')
     with app.test_client() as client:
         yield client
-        
-# Case 1: Successful delete a bid by changing status to deleted
-def test_delete_bid_success(client):
-   with patch('api.controllers.bid_controller.dbConnection') as mock_dbConnection:
-       mock_db = MagicMock()
-       mock_dbConnection.return_value = mock_db
-       mock_db['bids'].update_one = MagicMock()
-       mock_db['bids'].update_one.return_value = {'_id': '1ff45b42-b72a-464c-bde9-9bead14a07b9', 'status': 'deleted'}
-       response = client.put('/api/bids/1ff45b42-b72a-464c-bde9-9bead14a07b9')
-       assert response.status_code == 204
-       assert response.get_json() == None
 
+# Case 1: Successful delete a bid by changing status to deleted
+@patch('api.controllers.bid_controller.dbConnection')
+def test_delete_bid_success(mock_dbConnection, client):
+    mock_db = mock_dbConnection.return_value
+    mock_db['bids'].update_one.return_value = {
+        '_id': '1ff45b42-b72a-464c-bde9-9bead14a07b9',
+        'status': 'deleted'
+    }
+    response = client.put('/api/bids/1ff45b42-b72a-464c-bde9-9bead14a07b9')
+    assert response.status_code == 204
+    assert response.get_json() is None
 
 # Case 2: Failed to call database
-def test_delete_bid_find_error(client):
-    with patch('api.controllers.bid_controller.dbConnection') as mock_dbConnection:
-        mock_db = MagicMock()
-        mock_dbConnection.return_value = mock_db
-        mock_db['bids'].update_one = MagicMock(side_effect=ConnectionFailure)
-        response = client.put('/api/bids/1ff45b42-b72a-464c-bde9-9bead14a07b9')
-        assert response.status_code == 500
-        assert response.get_json() == {"Error": "Could not connect to database"}
-
+@patch('api.controllers.bid_controller.dbConnection')
+def test_delete_bid_find_error(mock_dbConnection, client):
+    mock_db = mock_dbConnection.return_value
+    mock_db['bids'].update_one.side_effect = ConnectionFailure
+    response = client.put('/api/bids/1ff45b42-b72a-464c-bde9-9bead14a07b9')
+    assert response.status_code == 500
+    assert response.get_json() == {"Error": "Could not connect to database"}
 
 # Case 3: Validation error
-def test_delete_bid_by_id_validation_error(client):
-    # Create a MagicMock object for mocking the valid_bid_id_schema object
-    mock_valid_bid_id_schema = MagicMock()
-
-    # Set up the side effect of valid_bid_id_schema.load to raise a ValidationError
-    mock_valid_bid_id_schema().load.side_effect = ValidationError('Invalid Bid ID')
-
-    # Patch the necessary functions and objects with the MagicMock object
-    with patch('api.controllers.bid_controller.valid_bid_id_schema', mock_valid_bid_id_schema):
-        # Call the endpoint with the desired URL
-        response = client.get('/api/bids/invalid_bid_id')
-
-    # Assertions
-    mock_valid_bid_id_schema().load.assert_called_once_with({'bid_id': 'invalid_bid_id'})
+@patch('api.controllers.bid_controller.valid_bid_id_schema.load')
+def test_get_bid_by_id_validation_error(mock_valid_bid_id_schema_load, client):
+    mock_valid_bid_id_schema_load.side_effect = ValidationError('Invalid Bid ID')
+    response = client.get('/api/bids/invalid_bid_id')
+    mock_valid_bid_id_schema_load.assert_called_once_with({'bid_id': 'invalid_bid_id'})
     assert response.status_code == 400
     assert response.get_json() == {"Error": "Invalid Bid ID"}
