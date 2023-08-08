30c8be1514a5738bc60905b7a8c8d091a5bbcbff
Lakorthus <julio28ve@gmail.com>
2023-08-08 15:40:56 +0100

    refactor: get bids with sort

--- tests/test_get_bids.py
+++ tests/test_get_bids.py
@@ -1,21 +1,35 @@
-"""
-This file contains tests for the GET /bids endpoint.
-"""
-
 from unittest.mock import patch
 
 
 # Case 1: Successful get
 @patch("api.controllers.bid_controller.db")
 def test_get_bids_success(mock_db, test_client, api_key):
-    mock_db["bids"].find.return_value = []
+    # Mock the find method
+    mock_db["bids"].find().skip().limit.return_value = [
+        {
+            "_id": "1ff45b42-b72a-464c-bde9-9bead14a07b9",
+            "bid_date": "2023-06-23",
+            "client": "Office for National Statistics",
+            "links": {
+                "questions": "/bids/faaf8ef5-5db4-459d-8d24-bc39492e1301/questions",
+                "self": "/bids/faaf8ef5-5db4-459d-8d24-bc39492e1301",
+            },
+            "status": "in_progress",
+            "tender": "Business Intelligence and Data Warehousing",
+        }
+    ]
 
-    response = test_client.get(
+    response_data = test_client.get(
         "/api/bids", headers={"host": "localhost:8080", "X-API-Key": api_key}
     )
-    mock_db["bids"].find.assert_called_once_with({"status": {"$ne": "deleted"}})
-    assert response.status_code == 200
-    assert response.get_json() == {"total_count": 0, "items": []}
+    assert response_data.status_code == 200
+
+    # Assert response structure
+    assert "count" in response_data
+    assert "total_count" in response_data
+    assert "limit" in response_data
+    assert "offset" in response_data
+    assert "data" in response_data
 
 
 # Case 2: Links prepended with hostname
@@ -41,7 +55,9 @@ def test_links_with_host(mock_db, test_client, api_key):
     assert response.status_code == 200
     assert response.get_json() == {
         "total_count": 1,
-        "items": [
+        "limit": 5,
+        "offset": 0,
+        "data": [
             {
                 "_id": "1ff45b42-b72a-464c-bde9-9bead14a07b9",
                 "bid_date": "2023-06-23",
