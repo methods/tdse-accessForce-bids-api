c132e72ce02b315c74f382c1deeb0f888321861e
piratejas <piratejas@gmail.com>
2023-08-10 09:48:57 +0100

    test: get bids with apikey

--- tests/integration/test_get_bids_with_apikey.py
+++ tests/integration/test_get_bids_with_apikey.py
@@ -1,8 +1,8 @@
-import pytest
+# import pytest
 import requests
 
 
-@pytest.mark.skip
+# @pytest.mark.skip
 def test_get_bids_with_api_key():
     auth_response = requests.get("http://localhost:5000/authorise/")
 
@@ -12,4 +12,10 @@ def test_get_bids_with_api_key():
 
     get_response = requests.get("http://localhost:8080/api/bids", headers=headers)
 
+    assert auth_response.status_code == 200
+    assert auth_response.json()["API_KEY"] is not None
     assert get_response.status_code == 200
+    # 1 bid out of 10 has "status": "deleted"
+    assert get_response.json()["total_count"] == 9
+    assert get_response.json()["limit"] == 20
+    assert get_response.json()["offset"] == 0
